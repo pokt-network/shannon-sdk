cc1037c0ea951e485c3efcd7fa5f66c26e4fa3c1
Arash <23505281+adshmh@users.noreply.github.com>
2024-08-20 10:05:38 -0400

    Update relay.go
    Co-authored-by: Daniel Olshansky <olshansky.daniel@gmail.com>

--- relay.go
+++ relay.go
@@ -25,10 +25,16 @@ func init() {
 func initCosmosSDKConfig() {
 	// Set prefixes
 	accountPubKeyPrefix := app.AccountAddressPrefix + "pub"
+	validatorAddressPrefix := app.AccountAddressPrefix + "valoper"
+	validatorPubKeyPrefix := app.AccountAddressPrefix + "valoperpub"
+	consNodeAddressPrefix := app.AccountAddressPrefix + "valcons"
+	consNodePubKeyPrefix := app.AccountAddressPrefix + "valconspub"
 
 	// Set and seal config
-	config := cosmossdk.GetConfig()
+	config := sdk.GetConfig()
 	config.SetBech32PrefixForAccount(app.AccountAddressPrefix, accountPubKeyPrefix)
+	config.SetBech32PrefixForValidator(validatorAddressPrefix, validatorPubKeyPrefix)
+	config.SetBech32PrefixForConsensusNode(consNodeAddressPrefix, consNodePubKeyPrefix)
 	config.Seal()
 }
 
